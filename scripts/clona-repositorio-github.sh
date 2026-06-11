cd75978a3e51bfe849431705f781891c363c7658
Arthur Gabriel Fabrim Alonso <arthur.fabrin028@gmail.com>
2026-06-11 10:29:26 -0300

    Adiciona permissão de leitura arquivos .env

--- scripts/clona-repositorio-github.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Clona o repositorio remoto
-# git clone -b <branch-name> <repository-url> <destino>
-sudo git clone -b main https://github.com/Arthwh/task_2_receitas.git ./application
