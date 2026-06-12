8a5c1c7aa0a7238e5d6f1ca9ef30318f15b10a54
Arthur Gabriel Fabrim Alonso <arthur.fabrin028@gmail.com>
2026-06-12 12:04:08 -0300

    Altera migração 2 devido a erro de execução nos testes. Utiliza sintaxe comum para Postgresql e H2

--- src/main/resources/db/migration/V2__add_administrator_profile.sql
+++ src/main/resources/db/migration/V2__add_administrator_profile.sql
@@ -1,3 +1,5 @@
 INSERT INTO usuario (nome, login, senha, situacao)
-VALUES ('Administrador', 'administrador@administrador.com', '$2a$10$ns1IzzY4SnP/JAAxUuhnF.EYYTqcsQTkbz/BlKhPVgvt6p53F9LGe', 'ATIVO')
-ON CONFLICT (login) DO NOTHING;
+SELECT 'Administrador', 'administrador@administrador.com', '$2a$10$ns1IzzY4SnP/JAAxUuhnF.EYYTqcsQTkbz/BlKhPVgvt6p53F9LGe', 'ATIVO'
+    WHERE NOT EXISTS (
+    SELECT 1 FROM usuario WHERE login = 'administrador@administrador.com'
+);
