33b04b175112ccd84d8239db06e22c7f5d7f82c6
Arthur Gabriel Fabrim Alonso <arthur.fabrin028@gmail.com>
2026-06-30 16:29:47 -0300

    Teste erro linter

--- src/main/java/com/arthwh/registroreceitas/service/UsuarioService.java
+++ src/main/java/com/arthwh/registroreceitas/service/UsuarioService.java
@@ -4,7 +4,9 @@ import com.arthwh.registroreceitas.dto.UsuarioUpdateDto;
 import com.arthwh.registroreceitas.exception.UsuarioNotFoundException;
 import com.arthwh.registroreceitas.model.Usuario;
 import com.arthwh.registroreceitas.repository.UsuarioRepository;
+
 import java.util.List;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -12,50 +14,46 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Service
 public class UsuarioService {
-  private final UsuarioRepository usuarioRepository;
-  private final PasswordEncoder passwordEncoder;
+    private final UsuarioRepository usuarioRepository;
+    private final PasswordEncoder passwordEncoder;
 
-  public UsuarioService(UsuarioRepository usuarioRepository, PasswordEncoder passwordEncoder) {
-    this.usuarioRepository = usuarioRepository;
-    this.passwordEncoder = passwordEncoder;
-  }
+    public UsuarioService(UsuarioRepository usuarioRepository, PasswordEncoder passwordEncoder) {
+        this.usuarioRepository = usuarioRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 
-  public Usuario getUsuarioById(int id) {
-    return usuarioRepository.findById(id).orElseThrow(UsuarioNotFoundException::new);
-  }
+    public Usuario getUsuarioById(int id) {
+        return usuarioRepository.findById(id).orElseThrow(UsuarioNotFoundException::new);
+    }
 
-  public Usuario getUsuarioByLogin(String login) {
-    return usuarioRepository.findByLogin(login).orElseThrow(UsuarioNotFoundException::new);
-  }
+    public Usuario getUsuarioByLogin(String login) {
+        return usuarioRepository.findByLogin(login).orElseThrow(UsuarioNotFoundException::new);
+    }
 
-  public List<Usuario> getUsuarios() {
-    return usuarioRepository.findAll();
-  }
+    public List<Usuario> getUsuarios() {
+        return usuarioRepository.findAll();
+    }
 
-  public Usuario updateUsuario(UsuarioUpdateDto usuarioDto) {
-    Usuario usuario =
-        usuarioRepository.findById(usuarioDto.id()).orElseThrow(UsuarioNotFoundException::new);
+    public Usuario updateUsuario(UsuarioUpdateDto usuarioDto) {
+        Usuario usuario =
+                usuarioRepository.findById(usuarioDto.id()).orElseThrow(UsuarioNotFoundException::new);
 
-    String senhaCriptografada = passwordEncoder.encode(usuarioDto.senha());
-    usuario.setNome(usuarioDto.nome());
-    usuario.setSenha(senhaCriptografada);
+        String senhaCriptografada = passwordEncoder.encode(usuarioDto.senha());
+        usuario.setNome(usuarioDto.nome());
+        usuario.setSenha(senhaCriptografada);
 
-    Usuario usuarioAtualizado = usuarioRepository.save(usuario);
-    log.info("Usuário ID: {} atualizado com sucesso.", usuarioAtualizado.getId());
+        Usuario usuarioAtualizado = usuarioRepository.save(usuario);
+        log.info("Usuário ID: {} atualizado com sucesso.", usuarioAtualizado.getId());
 
-    return usuarioAtualizado;
-  }
+        return usuarioAtualizado;
+    }
 
-  public Usuario deleteUsuario(int id) {
-    Usuario usuario = usuarioRepository.findById(id).orElseThrow(UsuarioNotFoundException::new);
+    public Usuario deleteUsuario(int id) {
+        Usuario usuario = usuarioRepository.findById(id).orElseThrow(UsuarioNotFoundException::new);
 
-    usuarioRepository.delete(usuario);
-    log.info("Usuário ID: {} excluído com sucesso.", id);
+        usuarioRepository.delete(usuario);
+        log.info("Usuário ID: {} excluído com sucesso.", id);
 
-    return usuario;
-  }
-
-//  public void naoFazNada(){
-//
-//  }
+        return usuario;
+    }
 }
